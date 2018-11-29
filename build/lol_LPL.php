b3a9858526e36238a64939ab44023b729741aa52
Dunamic <44893395+Dunamic@users.noreply.github.com>
2018-11-30 00:12:36 +0100

    Update lol_LPL.php

--- build/lol_LPL.php
+++ build/lol_LPL.php
@@ -19,7 +19,10 @@
       <?php include('Menu.php'); ?>
 
       <div class="col-md order-md-1 h-100 content">
-        LPL
+        <h1>LPL
+        </h1>
+        <p2>The Tencent League of Legends Pro League (LPL) is the premier League of Legends esports competition in China. The LPL features 14 teams split into two divisions of 7 teams each.
+        </p2>
       </div>
     </div>
   </div>
