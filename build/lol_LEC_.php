802f5a20b26fa972d57026b33f9d7bb398e8d54f
Dunamic <44893395+Dunamic@users.noreply.github.com>
2018-11-29 22:29:40 +0100

    trololololo

--- build/lol_LEC_.php
+++ build/lol_LEC_.php
@@ -19,7 +19,14 @@
       <?php include('Menu.php'); ?>
 
       <div class="col-md order-md-1 h-100 content">
-      LEC
+        Welcome to League of Legends European Championship. LEC is the name for our brand new European league, but you’ll be familiar with our ethos. We move the game forward, proudly united by our passion for world class competitive play and incredible spectacle.
+
+  We do that by welcoming everyone from everywhere. Our league is incredibly diverse, bringing together fans and professional players from over 40 countries. You see, our shared love for the game is what makes this league unique.
+
+  It’s what pushes our stars to break the meta and evolve the sport. It’s what creates the electric atmosphere and the unfolding drama every week. It’s what forges unforgettable moments, and makes LEC the most engaging esports league out there.
+  LEC is your brand. We’ve been in the studio, we’ve interviewed fans from all over. We’ve created something bigger, better and most importantly, uniquely us. Hardcore, humorous but just as competitive. A brand that embraces our diverse clash of cultures. A brand that pushes the boundaries. A brand that celebrates you, our community.
+
+That means our attitude is yours. Like you, we’re proud to be ourselves and carve out our own path. We never chase the crowd but set the trends that others follow. It’s not just our pro players dropping new strats on the Rift each week – as a community we’re never afraid to dream big, be bold and ambitious. We’re always challenging what’s expected of us and never settling for the norm. Welcome.
       </div>
     </div>
   </div>
