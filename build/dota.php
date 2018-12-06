fe1d8db1449d93faa6f60fc4752719e4ff898cd4
Dunamic <44893395+Dunamic@users.noreply.github.com>
2018-12-07 00:00:51 +0100

    Dota and LoL conent done
    one less game to go

--- build/dota.php
+++ build/dota.php
@@ -19,8 +19,13 @@
       <?php include('Menu.php'); ?>
 
       <div class="col-md order-md-1 h-100 content" id="content">
-        Dota 2 – gra komputerowa z gatunku action RTS, (zwanego także MOBA), która została stworzona i wydana przez Valve Corporation. Oparta jest na modelu free-to-play, i używa po raz pierwszy silnika Source 2. Jest samodzielną kontynuacją modyfikacji Defense of the Ancients do gry Warcraft III: Reign of Chaos i dodatku Warcraft III: The Frozen Throne. Została wydana na Microsoft Windows, macOS oraz Linuksa.
-Została oficjalnie zapowiedziana 13 października 2010 roku w serwisie Game Informer. Przez dwa lata gra była w fazie beta testów, a 9 lipca 2013 roku nastąpiła jej oficjalna premiera[10].
+        <img class="lol-content-image" src="Assets/Images/dota2.jpg" alt="dota">
+            <h1>
+              Dota 2 is a sequel to Dota or Defense of the Ancients, which was a user-created map for Warcraft 3. For more on the evolution of Dota see Dota History. Dota and Dota 2 have been classified by many as MOBA games, or Multiplayer Online Battle Arena games.
+            </h1>
+            <p>
+              NOTE: Dota 2 is a deep and robust game with a steep learning curve. It is highly recommended that you explore the tutorial mode, play with bots, and  read some introductions before you jump in and play. The average game of Dota 2 lasts 45-50 minutes. It is highly detrimental to other player's game experience if you leave before finishing the game and is heavily frowned upon.
+            </p>
       </div>
     </div>
   </div>
